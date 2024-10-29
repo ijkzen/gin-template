fd8cf3cc8248afdf03723a71955e2b5252a40a06
ijkzen <ijkzen@outlook.com>
2024-10-29 23:14:54 +0800

    update rename script

--- rename.sh
+++ rename.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
+if [ $# != 2 ]; then
     echo "please pass new module name"
 else
-    sed -i "" "s#github.com/ijkzen/gin-template#$1#g" go.mod
-    find . -type f -iname "*.go" | xargs sed -i "" "s#github.com/ijkzen/gin-template#$1#g"
-    sed -i "" "s#template#$1#g" .harness/Build.yaml
+    if [ "$(uname)" == "Darwin" ]; then
+        sed -i "" "s#github.com/ijkzen/gin-template#$1#g" go.mod
+        find . -type f -iname "*.go" | xargs sed -i "" "s#github.com/ijkzen/gin-template#$1#g"
+        find . -type f -iname "*.json" | xargs sed -i "" "s#angular-template#$2#g"
+        find . -type f -iname "*.ts" | xargs sed -i "" "s#angular-template#$2#g"
+        sed -i "" "s#template#$2#g" .harness/Build.yaml
+    elif [ "$(uname)" == "Linux" ]; then
+        sed -i "s#github.com/ijkzen/gin-template#$1#g" go.mod
+        find . -type f -iname "*.go" | xargs sed -i "s#github.com/ijkzen/gin-template#$1#g"
+        find . -type f -iname "*.json" | xargs sed -i "s#angular-template#$2#g"
+        find . -type f -iname "*.ts" | xargs sed -i "s#angular-template#$2#g"
+        sed -i "s#template#$2#g" .harness/Build.yaml
+    else
+        echo "unsupported platform"
+    fi
 fi
