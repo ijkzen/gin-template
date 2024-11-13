017fc247fd3bd0b8264aed710ce5315c2285307f
ijkzen <ijkzen@outlook.com>
2024-11-14 00:07:38 +0800

    add toast and confirm dialog

--- web/src/app/service/toast.service.ts
+++ web/src/app/service/toast.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
+
+  showToast(message: string, action: string = 'Close') {
+    this.snackBar.open(message, action, {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
 }
--- web/src/app/shared/confirm-dialog/confirm-dialog.component.ts
+++ web/src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
-  standalone: true,
-  imports: [],
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.scss'
+  styleUrls: ['./confirm-dialog.component.scss'],
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule]
 })
 export class ConfirmDialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string },
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>
+  ) { }
 
+  get title() {
+    return this.data.title;
+  }
+
+  get message() {
+    return this.data.message;
+  }
+
+
+  cancel() {
+    this.dialogRef.close(false);
+  }
+
+  confirm() {
+    this.dialogRef.close(true);
+  }
 }
