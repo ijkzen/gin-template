ab4e0a2c578b2ff75286be1a18e6e674109bba20
ijkzen <ijkzen@outlook.com>
2024-11-14 20:32:23 +0800

    support to load icon from local

--- web/src/app/page/toolbar/toolbar.component.ts
+++ web/src/app/page/toolbar/toolbar.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { StyleManagerService } from '../../service/style-manager.service';
+import { MatButtonModule } from '@angular/material/button';
+import { ProgressBarService } from '../../service/progress-bar.service';
+import { ToastService } from '../../service/toast.service';
 
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbar, MatIconModule],
+  imports: [MatToolbar, MatIconModule, MatButtonModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
@@ -16,18 +19,32 @@ export class ToolbarComponent {
   isDark = false;
 
   constructor(
-    private sytleManager: StyleManagerService
+    private styleManager: StyleManagerService,
+    private progressBarService: ProgressBarService,
+    private toastService: ToastService
   ) {
 
   }
 
   switchTheme() {
     if (this.isDark) {
-      this.sytleManager.removeStyle("theme");
+      this.styleManager.removeStyle("theme");
     } else {
-      this.sytleManager.setStyle("theme", "magenta-violet.css");
+      this.styleManager.setStyle("theme", "magenta-violet.css");
     }
 
     this.isDark = !this.isDark;
+  }
+
+  switchProgressBar() {
+    if (this.progressBarService.showing()) {
+      this.progressBarService.hide()
+    } else {
+      this.progressBarService.show()
+    }
+  }
+
+  showToast() {
+    this.toastService.showToast("这是一个通知🥰")
   }
 }
