0a71caf001a1924719fb99538a4b1ecdc68cf504
ijkzen <ijkzen@outlook.com>
2024-11-14 22:19:14 +0800

    update material form-field style

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,17 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { httpInterceptor } from './interceptor/http.interceptor';
-import { provideHttpClient } from '@angular/common/http';
-import { withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([httpInterceptor]))
+    provideHttpClient(withInterceptors([httpInterceptor])),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}
+    }
   ]
 };
