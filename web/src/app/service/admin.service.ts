8df91477f576fc6c279a36bfa5b422a7fdfbac3d
ijkzen <ijkzen@outlook.com>
2024-11-18 22:32:45 +0800

    add generic type for http request

---
+++ web/src/app/service/admin.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Response } from './bean/response/response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getMenuList(): Observable<string[]> {
+    return this.handleRequest(this.http.get<Response<string[]>>("/api/admin/menu/list"));
+  }
+
+  private handleRequest<T>(ob: Observable<Response<T>>): Observable<T> {
+    return ob.pipe(
+      map(response => {
+        if (response.isSuccess()) {
+          return response.data;
+        }
+        throw new Error(response.errMessage);
+      })
+    );
+  }
+}
