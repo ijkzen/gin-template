19ec43bdf50d4c11af9a0421014d4efd3954d1bb
ijkzen <ijkzen@outlook.com>
2025-06-19 21:36:40 +0800

    Add service worker update handling and progress bar management

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
+import { ApplicationRef, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ProgressBarService } from "./service/progressbar.service";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { CommonModule } from "@angular/common";
+import { SwUpdate, VersionReadyEvent } from "@angular/service-worker";
+import { filter } from "rxjs/internal/operators/filter";
+import { concat, first, interval } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -13,9 +16,40 @@ import { CommonModule } from "@angular/common";
 export class AppComponent {
   showProgressBar: boolean = false;
 
-  constructor(private progressBarService: ProgressBarService) {
+  constructor(
+    private progressBarService: ProgressBarService,
+    private swUpdate: SwUpdate,
+    private appRef: ApplicationRef
+  ) {
     this.progressBarService.progressBar$.subscribe((show) => {
       this.showProgressBar = show;
+    });
+
+    swUpdate.versionUpdates
+      .pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === "VERSION_READY")
+      )
+      .subscribe((evt) => {
+        if (evt) {
+          document.location.reload();
+        }
+      });
+
+    const appIsStable$ = this.appRef.isStable.pipe(
+      first((isStable) => isStable === true)
+    );
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+    everySixHoursOnceAppIsStable$.subscribe(async () => {
+      if (this.swUpdate.isEnabled) {
+        try {
+          const updateFound = await this.swUpdate.checkForUpdate();
+        } catch (err) {
+          console.error("Failed to check for updates:", err);
+        }
+      } else {
+        console.warn("Service Worker is not enabled.");
+      }
     });
   }
 }
