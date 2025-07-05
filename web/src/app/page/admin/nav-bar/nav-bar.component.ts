16bd8c028792dc5e581fcd3a1f125e9c61dccfe6
ijkzen <ijkzen@outlook.com>
2025-07-05 20:39:33 +0800

    refactor: enhance admin navigation layout and improve component structure

--- web/src/app/page/admin/nav-bar/nav-bar.component.ts
+++ web/src/app/page/admin/nav-bar/nav-bar.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core';
-import {MatDividerModule} from '@angular/material/divider'
-import { RouterOutlet } from '@angular/router';
-import { Nav } from '../../../service/bean/nav';
-import { NavBarItemComponent } from '../nav-bar-item/nav-bar-item.component';
+import { Component } from "@angular/core";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { Router, RouterOutlet } from "@angular/router";
+import { Nav } from "../../../service/bean/nav";
+import { NavService } from "../../../service/nav.service";
+import { ScreenService } from "../../../service/screen.service";
+import { IconButtonComponent } from "../../../shared/icon-button/icon-button.component";
+import { NavBarItemComponent } from "../nav-bar-item/nav-bar-item.component";
+
 @Component({
-    selector: 'app-nav-bar',
-    imports: [MatDividerModule, RouterOutlet, NavBarItemComponent],
-    templateUrl: './nav-bar.component.html',
-    styleUrl: './nav-bar.component.scss'
+  selector: "app-nav-bar",
+  imports: [
+    MatDividerModule,
+    RouterOutlet,
+    NavBarItemComponent,
+    MatToolbarModule,
+    MatMenuModule,
+    IconButtonComponent,
+  ],
+  templateUrl: "./nav-bar.component.html",
+  styleUrl: "./nav-bar.component.scss",
 })
 export class NavBarComponent {
-  navList : Nav[] = [
-    {name:'nav1',url:'/admin/nav/nav1'},
-    {name:'nav2',url:'/admin/nav/nav2'},
-    {name:'nav3',url:'/admin/nav/nav3'}
-  ]
+  navList: Nav[] = [
+    { name: "nav1", url: "/admin/nav/nav1" },
+    { name: "nav2", url: "/admin/nav/nav2" },
+    { name: "nav3", url: "/admin/nav/nav3" },
+  ];
+
+  constructor(
+    private screenService: ScreenService,
+    private router: Router,
+    private navService: NavService
+  ) {
+    this.navService.select("/admin/nav/nav1");
+  }
+
+  isDesktop(): boolean {
+    return this.screenService.isLargeScreen();
+  }
+
+  onRouter(url: string): void {
+    this.router.navigate([url]);
+    this.navService.select(url);
+  }
+
+  isSelected(url: string): boolean {
+    return this.navService.isSelected(url);
+  }
 }
