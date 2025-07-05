674efd5bc396098d735af069e61fec99ff973bc6
ijkzen <ijkzen@outlook.com>
2025-07-05 19:44:05 +0800

    refactor: reorganize imports and improve layout structure in admin component

--- web/src/app/page/admin/admin.component.ts
+++ web/src/app/page/admin/admin.component.ts
@@ -1,48 +1,56 @@
-import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { ThemeSwitchIconComponent } from "../../shared/theme-switch-icon/theme-switch-icon.component";
-import { WebsocketService } from '../../service/websocket.service';
-import { MatDialog } from '@angular/material/dialog';
-import { ChangePasswordDialogComponent } from '../changepw/change-password-dialog.component';
-import { AuthService } from '../../service/auth.service';
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialog } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { AuthService } from "../../service/auth.service";
+import { ScreenService } from "../../service/screen.service";
+import { WebsocketService } from "../../service/websocket.service";
 import { IconButtonComponent } from "../../shared/icon-button/icon-button.component";
+import { ThemeSwitchIconComponent } from "../../shared/theme-switch-icon/theme-switch-icon.component";
+import { ChangePasswordDialogComponent } from "../changepw/change-password-dialog.component";
 
 @Component({
-    selector: 'app-admin',
-    imports: [MatTabsModule, RouterLink, RouterOutlet, MatIconModule, MatButtonModule, ThemeSwitchIconComponent, IconButtonComponent],
-    templateUrl: './admin.component.html',
-    styleUrl: './admin.component.scss'
+  selector: "app-admin",
+  imports: [
+    MatTabsModule,
+    RouterLink,
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    ThemeSwitchIconComponent,
+    IconButtonComponent,
+  ],
+  templateUrl: "./admin.component.html",
+  styleUrl: "./admin.component.scss",
 })
 export class AdminComponent {
   routerList = [
     {
       name: "Nav",
-      path: "./nav"
+      path: "./nav",
     },
     {
       name: "Menu1",
-      path: "./menu1"
+      path: "./menu1",
     },
     {
       name: "Menu2",
-      path: "./menu2"
+      path: "./menu2",
     },
     {
       name: "Menu3",
-      path: "./menu3"
-    }
-  ]
+      path: "./menu3",
+    },
+  ];
 
   constructor(
     private websocketService: WebsocketService,
     private dialog: MatDialog,
-    private authService: AuthService
-  ) {
-  }
-
+    private authService: AuthService,
+    private screenService: ScreenService
+  ) {}
 
   showChangePasswordDialog() {
     this.dialog.open(ChangePasswordDialogComponent, {
@@ -53,5 +61,9 @@ export class AdminComponent {
   logout() {
     this.authService.logout();
     location.reload();
+  }
+
+  isDesktop(): boolean {
+    return this.screenService.isLargeScreen();
   }
 }
